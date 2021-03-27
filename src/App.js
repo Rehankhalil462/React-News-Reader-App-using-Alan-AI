c4c2f7586936737b16ae607767c48c82dbf22cc0
Rehan Khalil <rehankhalil462@gmail.com>
2021-03-27 06:50:05 +0500

    created ALAN AI Conversational Reading App in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import NewsCards from './components/NewsCards/NewsCards.component';
+import Footer from './components/Footer/Footer';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import useStyles from './app.styles.js';
+import wordsToNumbers from 'words-to-numbers';
+import Modal from './components/Modal/Modal.component';
+import { Typography } from '@material-ui/core';
+import AlanLogo from './AlanLogo.png';
 
-function App() {
+const App = () => {
+  const classes = useStyles();
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const alanKey =
+    'b7c5003f6518b3a80b4d1ca9f98631012e956eca572e1d8b807a3e2338fdd0dc/stage';
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newsHeadlines') {
+          // console.log('these are the articles: ', articles);
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevactiveArticle) => prevactiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again!');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening');
+          } else {
+            alanBtn().playText('Please try that again!');
+          }
+        }
+      },
+    });
+  }, []);
+
+  // <img src={AlanLogo} className={classes.alanLogo} alt='Alan Logo' />
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant='h5' component='h2' style={{ fontSize: 15 }}>
+                Try saying: <br />
+                <br />
+                Open the article number 2 or 3 or 4 etc
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant='h5' component='h2' style={{ fontSize: 15 }}>
+                Try saying: <br />
+                <br />
+                Go back
+                <br />
+                or
+                <br />
+                Give me the Instructions
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img src={AlanLogo} className={classes.alanLogo} alt='logo' />
+      </div>{' '}
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
