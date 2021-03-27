6a507819b1db7a0676f5e53b40bacfa7da35a4bd
Rehan Khalil <rehankhalil462@gmail.com>
2021-03-27 08:37:05 +0500

    removing a bug

--- src/components/NewsCards/NewsCards.component.jsx
+++ src/components/NewsCards/NewsCards.component.jsx
@@ -82,7 +82,7 @@ const NewsCards = ({ articles, activeArticle }) => {
       >
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
-            <NewsCard activeArticle={activeArticle} article={article} i={i} />;
+            <NewsCard activeArticle={activeArticle} article={article} i={i} />
           </Grid>
         ))}
       </Grid>
